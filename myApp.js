6eb4f185d23782229866ca07152b5d792aadf6cf
Tyler <realpsych22@gmail.com>
2020-03-29 22:45:10 -0500

    querychain

--- myApp.js
+++ myApp.js
@@ -303,9 +303,11 @@ var removeManyPeople = function(done) {
 
 var queryChain = function(done) {
   var foodToSearch = "burrito";
-  
-  done(null/*, data*/);
-};
+    Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age:0}).exec(function(err, data){
+      if(err) return done(err);
+       done(null, data);
+    });
+  };
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
